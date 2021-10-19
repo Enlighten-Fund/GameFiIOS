d2205cacb2ba418b4426700772d540c58ce1d43d
chenlu <javachenwork@163.com>
2021-10-19 17:29:43 +0800

    网络请求的构建

---
+++ gamefiIOS/gamefiIOS/General/GFNavController.h
@@ -0,0 +1,16 @@
+//
+//  GFNavController.h
+//  gamefiIOS
+//
+//  Created by harden on 2021/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GFNavController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
