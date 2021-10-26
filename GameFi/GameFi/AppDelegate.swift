6a9396a51e1584610fdf1f75ceaf01f1cb1b6c15
chenlu <javachenwork@163.com>
2021-10-26 19:00:29 +0800

    tabbar添加

--- GameFi/GameFi/AppDelegate.swift
+++ GameFi/GameFi/AppDelegate.swift
@@ -9,11 +9,14 @@ import UIKit
 import Amplify
 import AmplifyPlugins
 import AWSPluginsCore
+import ESTabBarController_swift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window?.rootViewController = ViewController.init()
+        
+    
+    self.window?.rootViewController = self.tabbarVC
 //        do {
             
             
@@ -119,6 +122,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
         window.backgroundColor = UIColor.white
         return window
+    }()
+    
+    lazy var tabbarVC: ESTabBarController? = {
+        let tabBarController = ESTabBarController()
+        let v1 = ViewController()
+        let v2 = ViewController()
+        let v3 = ViewController()
+        let v4 = ViewController()
+        let v5 = ViewController()
+        
+        v1.tabBarItem = ESTabBarItem.init(title: "Home", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
+        v2.tabBarItem = ESTabBarItem.init(title: "Find", image: UIImage(named: "find"), selectedImage: UIImage(named: "find_1"))
+        v3.tabBarItem = ESTabBarItem.init(title: "Photo", image: UIImage(named: "photo"), selectedImage: UIImage(named: "photo_1"))
+        v4.tabBarItem = ESTabBarItem.init(title: "Favor", image: UIImage(named: "favor"), selectedImage: UIImage(named: "favor_1"))
+        v5.tabBarItem = ESTabBarItem.init(title: "Me", image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"))
+        
+        let n1 = GFNavController.init(rootViewController: v1)
+        let n2 = GFNavController.init(rootViewController: v2)
+        let n3 = GFNavController.init(rootViewController: v3)
+        let n4 = GFNavController.init(rootViewController: v4)
+        let n5 = GFNavController.init(rootViewController: v5)
+        
+        v1.title = "Home"
+        v2.title = "Find"
+        v3.title = "Photo"
+        v4.title = "List"
+        v5.title = "Me"
+        tabBarController.viewControllers = [n1, n2, n3, n4, n5]
+        return tabBarController
     }()
 }
 
